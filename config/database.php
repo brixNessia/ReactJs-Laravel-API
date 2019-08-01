87ddce8d4b69a878e1ef7710c8dc72d07c6ce73d
nessia.brb <nessia.brb@sprobe.com>
2019-08-01 14:06:50 +0800

    add database

--- config/database.php
+++ config/database.php
@@ -3,12 +3,7 @@
 use Illuminate\Support\Str;
 
 
-$url = parse_url(getenv("DATABASE_URL"));
-
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
+$heroku_db_url = parse_url(env('DATABASE_URL', "postgres://zyvwmligpdnwhe:e46e60dd0f88ebae3143f431ca71664e869aeb305e9d3c0cc85863c6d6f8f6bd@ec2-174-129-209-212.compute-1.amazonaws.com:5432/d6e5vgkoocgt36"));
 
 return [
 
@@ -71,12 +66,12 @@ return [
             ]) : [],
         ],
 
-        'pgsql' => [
+        'pg-heroku' => [
             'driver'   => 'pgsql',
-            'host'     => $host,
-            'database' => $database,
-            'username' => $username,
-            'password' => $password,
+            'host'     => $heroku_db_url['host'],
+            'database' => substr($heroku_db_url['path'], 1),
+            'username' => $heroku_db_url['user'],
+            'password' => $heroku_db_url['pass'],
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
