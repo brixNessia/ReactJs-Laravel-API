80b2e640154b680b760d8b11ebcd301e4c29417c
brixNessia <33595897+brixNessia@users.noreply.github.com>
2019-08-02 21:53:04 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('currentUser','HomeController@currentUser');
+Route::middleware('auth:api')->get('/currentUser', function (Request $request) {
+    return response()->json(['user' => 'fuck');
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
