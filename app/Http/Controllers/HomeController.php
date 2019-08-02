05355d512a33ce52bf43cb8825c25faf84d4ed21
brixNessia <33595897+brixNessia@users.noreply.github.com>
2019-08-02 20:47:47 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -24,5 +25,18 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+    
+    public function currentUser()
+    {
+        $user = Auth::user();
+        
+        return response()->json([
+            'result' => [
+                'user_id' => $user->id,
+                'user_name' => $user->name,
+            ]
+        ]);
+        
     }
 }
