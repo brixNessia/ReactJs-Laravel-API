11f5d9c23133786ddb0db690ff04ba6ae55f7d30
nessia.brb <nessia.brb@sprobe.com>
2019-08-07 17:29:59 +0800

    add request

--- app/Http/Requests/SaveUserRequest.php
+++ app/Http/Requests/SaveUserRequest.php
@@ -26,7 +26,7 @@ class SaveUserRequest extends FormRequest
         return [
             'first_name' => 'required',
             'last_name' => 'required',
-            'email' => 'required|email|unique:users,email',
+            'email' => 'required|email|unique:users',
             'password' => 'required',
         ];
     }
