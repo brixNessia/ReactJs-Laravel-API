c3780dc91c050db13cca144821adae3ea8b2ebdd
nessia.brb <nessia.brb@sprobe.com>
2019-08-08 10:08:10 +0800

    add unique email

--- app/Http/Requests/SaveUserRequest.php
+++ app/Http/Requests/SaveUserRequest.php
@@ -26,7 +26,7 @@ class SaveUserRequest extends FormRequest
         return [
             'first_name' => 'required',
             'last_name' => 'required',
-            'email' => 'required|email|unique:users',
+            'email' => 'required|email|unique:pgsql.schema.users',
             'password' => 'required',
         ];
     }
