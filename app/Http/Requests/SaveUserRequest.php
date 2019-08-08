d98625f41ba4c1bd57addbcffc49dacfb240037d
nessia.brb <nessia.brb@sprobe.com>
2019-08-08 10:12:24 +0800

    Remove validation

--- app/Http/Requests/SaveUserRequest.php
+++ app/Http/Requests/SaveUserRequest.php
@@ -26,7 +26,7 @@ class SaveUserRequest extends FormRequest
         return [
             'first_name' => 'required',
             'last_name' => 'required',
-            'email' => 'required|email|unique:pgsql.schema.users',
+            'email' => 'required|email',
             'password' => 'required',
         ];
     }
